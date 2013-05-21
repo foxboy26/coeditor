7c39f519107c5ee66e28f03b2f1b82fd652197c3
Xixi Xiao <xixi@ip-10-245-46-247.us-west-2.compute.internal>
2013-05-21 01:21:11 +0000

    Change mysql password

--- web/src/db/Config.java
+++ web/src/db/Config.java
@@ -4,5 +4,5 @@ public class Config {
 	public static String jdbcDriver = "com.mysql.jdbc.Driver";
 	public static String connectionURL = "jdbc:mysql://localhost:3306/coeditor";
 	public static String username = "root";
-	public static String password = "";
+	public static String password = "cse223b";
 }
