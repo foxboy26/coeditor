f6798bb54cde43ec67048429c4ed27fb273d1e3d
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2013-06-02 17:46:06 -0700

    Remove unnecessary function

--- src/coeditor/SingleChange.java
+++ src/coeditor/SingleChange.java
@@ -17,26 +17,6 @@ public class SingleChange extends Change {
 	  this.length = 1;
 	}
 	
-	static public SingleChange merge(SingleChange lhs, SingleChange rhs) {
-		SingleChange merged = null;
-		
-		if (lhs.type == Change.NEW && rhs.type == Change.NEW) {
-			
-		} else if (lhs.type == Change.NEW && rhs.type == Change.OLD) {
-			merged = lhs;
-		} else if (lhs.type == Change.OLD && rhs.type == Change.NEW) {
-			merged = rhs;
-		} else {
-			if (lhs.pos == rhs.pos) {
-				merged = lhs;
-			}
-		}
-		
-		System.out.println(merged);
-		
-		return merged;
-	}
-	
 	public String toString() {
 		if (type == Change.NEW)
 			return Character.toString(this.c);
